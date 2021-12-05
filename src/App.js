526c0f3bda49960455029a9dd4fa407944192dd8
Dave Burke <dburke84@gmail.com>
2021-12-05 12:19:53 -0600

    Add sliders

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
       <header className="App-header">
         <h1>Coffee Calc</h1>
       </header>
+      <main className="App-main">
+        <fieldset>
+          <label for="cups">cups</label>
+          <input id="cups" type='range' min='0' max='12' value='10'/>
+        </fieldset>
+        <fieldset>
+          <label for="ratio">ratio</label>
+          <input id="ratio" type='range' min='10' max='18' value='17'/>
+        </fieldset>
+      </main>
     </div>
   );
 }
