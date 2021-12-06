2f79920a7bc7cd13c49daa10d0193bd586304bb3
Dave Burke <dburke84@gmail.com>
2021-12-05 18:00:31 -0600

    Add state for cups and ratio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cups, setCups] = useState(10)
+  const [ratio, setRatio] = useState(17)
   return (
     <div className="App">
       <header className="App-header">
@@ -8,12 +11,20 @@ function App() {
       </header>
       <main className="App-main">
         <fieldset>
-          <label for="cups">cups</label>
-          <input id="cups" type='range' min='0' max='12' value='10'/>
+          <label htmlFor="cups">cups</label>
+          <input
+            id="cups" type='range'
+            min='0' max='12'
+            value={cups} onChange={e => setCups(e.target.value)}/>
+            {cups}
         </fieldset>
         <fieldset>
-          <label for="ratio">ratio</label>
-          <input id="ratio" type='range' min='10' max='18' value='17'/>
+          <label htmlFor="ratio">ratio</label>
+          <input
+            id="ratio" type='range'
+            min='10' max='18'
+            value={ratio} onChange={e => setRatio(e.target.value)}/>
+            1/{ratio}
         </fieldset>
       </main>
     </div>
