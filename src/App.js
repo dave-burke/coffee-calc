5828f0e94a618c1a97866841d8387cf2f45a14de
Dave Burke <dburke84@gmail.com>
2021-12-06 21:44:07 -0600

    Use useMemo to calculate the result
    I don't think it's really necessary in this simple case, but it's a nice
    experiment.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import styled from 'styled-components'
 import Slider from './Slider'
 import Header from './Header'
@@ -21,17 +21,18 @@ function App() {
   const [cups, setCups] = useState(10)
   const [ratio, setRatio] = useState(17)
 
-  const calcCoffeeGrams = () => {
+  const coffeeGrams = useMemo(() => {
     const gramsWater = cups * 5 /* oz per cup */ * 30 /* grams per ounce */
     return Math.round(gramsWater / ratio)
-  }
+  }, [cups, ratio]);
+
   return (
     <StyledApp className="has-text-centered">
       <Header></Header>
       <Main className="ml-auto mr-auto">
         <Slider id="cups" label="Cups" min="1" max="12" value={cups} onChange={setCups}></Slider>
         <Slider id="ratio" label="Ratio" min="10" max="18" value={ratio} onChange={setRatio}></Slider>
-        <p>Use <Result className="ml-1 mr-1">{calcCoffeeGrams()}</Result> grams of coffee</p>
+        <p>Use <Result className="ml-1 mr-1">{coffeeGrams}</Result> grams of coffee</p>
       </Main>
     </StyledApp>
   );
