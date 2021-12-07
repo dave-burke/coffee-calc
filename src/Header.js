4b69ffc1a938b867d2f0905f3d3b717bb764703c
Dave Burke <dburke84@gmail.com>
2021-12-06 21:20:12 -0600

    Fix H1 style in header
    Now it feels a little extreme not to just use has-color-white but I
    still like the consistency of only using utility classes for layout.
    
    In reality, though, with scoped css the style is so close to the markup
    that it's probably not *that* important to keep them separate.
    
    "Keep components small and encapsulated" is probably more important than
    "keep style and markup separate", even if both are good rules.

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,16 @@ import styled from 'styled-components'
 
 const StyledHeader = styled.header`
   background-color: #2C4C66;
+`
+
+const H1 = styled.h1`
   color: white;
 `
 
 function Header() {
     return (
         <StyledHeader>
-            <h1 class="title is-1 p-4">Coffee Calc</h1>
+            <H1 className="title is-1 p-4">Coffee Calc</H1>
         </StyledHeader>
     )
 }
